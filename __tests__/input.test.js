6669f4c047147f91680f3ffd05d28104b4e3f4d9
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-03-04 17:58:34 +0900

    test: 구매 금액 관련 클래스 분리

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -1,23 +1,20 @@
-import App from "../src/step1/app.js";
+import Money from "../src/step1/model/Money.js";
 import MockInput from "../src/step1/view/MockInput.js";
 import { MONEY_ERROR_MESSAGE } from "../src/step1/constant/message.js";
 
 describe("구매 금액 입력 테스트", () => {
   test("입력 금액이 숫자가 아니면 예외가 발생한다.", async () => {
-    const app = new App({ input: new MockInput(["1000a"]) });
-    await expect(app.run()).rejects.toThrow(
+    await expect(() => new Money(NaN)).toThrow(
       MONEY_ERROR_MESSAGE.INPUT_NOT_NUMBER,
     );
   });
   test("입력 금액이 양의 정수가 아니면 예외가 발생한다", async () => {
-    const app = new App({ input: new MockInput(["3.14"]) });
-    await expect(app.run()).rejects.toThrow(
+    await expect(() => new Money(3.14)).toThrow(
       MONEY_ERROR_MESSAGE.INPUT_NOT_INTEGER,
     );
   });
   test("입력 금액이 1000원 단위가 아니면 예외가 발생한다", async () => {
-    const app = new App({ input: new MockInput(["500"]) });
-    await expect(app.run()).rejects.toThrow(
+    await expect(() => new Money(500)).toThrow(
       MONEY_ERROR_MESSAGE.INPUT_NOT_THOUSAND_UNIT,
     );
   });
