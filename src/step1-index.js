7ed22af3418e004314241c5cc5f27959b937c259
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-03-04 15:38:44 +0900

    feat: 구입 금액 입력 및 숫자 예외 처리 구구현

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,4 +1,56 @@
-/**
- * step 1의 시작점이 되는 파일입니다.
- * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+import readline from "readline";
+
+class Input {
+  #rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  async readLineAsync(message) {
+    return new Promise((resolve, reject) => {
+      this.#rl.question(message ?? "", (line) => {
+        this.#rl.close();
+        resolve(line);
+      });
+    });
+  }
+}
+
+export class MockInput {
+  #returnValue;
+
+  constructor(returnValue) {
+    this.#returnValue = returnValue;
+  }
+
+  async readLineAsync(message) {
+    return new Promise((resolve, reject) => {
+      resolve(this.#returnValue);
+    });
+  }
+}
+
+class App {
+  #input;
+
+  constructor({ input } = {}) {
+    this.#input = input ?? new Input();
+  }
+
+  async run() {
+    const test = await this.#input.readLineAsync("구입금액을 입력해 주세요.");
+
+    if (!Number(test)) {
+      throw new Error("[ERROR] 숫자를 입력해주세요.");
+    }
+
+    // if (test % 1000 !== 0) {
+    //   console.log("[ERROR]");
+    // }
+  }
+}
+
+const app = new App();
+app.run();
+
+export default App;
