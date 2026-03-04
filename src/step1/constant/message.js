b760b80f45626e6f7ffc27165170edfb6c1866ed
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-03-04 16:59:19 +0900

    test: 로또 번호 개수 테스트

--- src/step1/constant/message.js
+++ src/step1/constant/message.js
@@ -4,6 +4,10 @@ export const MONEY_ERROR_MESSAGE = Object.freeze({
   INPUT_NOT_THOUSAND_UNIT: "[ERROR] 금액은 1000원 단위로 입력해야 합니다.\n",
 });
 
+export const LOTTO_ERROR_MESSAGE = Object.freeze({
+  INPUT_NOT_SIX_NUMBER: "[ERROR] 로또 번호는 6개 이어야합니다.\n",
+});
+
 export const INPUT_MESSAGE = Object.freeze({
   PURCHASE_AMOUNT: "구입금액을 입력해주세요.\n",
   WINNING_NUMBER: "\n당첨 번호를 입력해주세요.\n",
