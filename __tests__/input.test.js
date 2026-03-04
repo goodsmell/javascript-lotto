dd47346212c4fcc6b6805d6e488fa7542b99113d
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-03-04 15:56:02 +0900

    test: 입력 금액이 양의 정수인지에 대한 예외 테스트

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -1,4 +1,5 @@
-import App, { MockInput } from "./../src/step1-index.js";
+import App from "../src/step1/app.js";
+import MockInput from "../src/step1/view/MockInput.js";
 
 describe("구매 금액 입력 테스트", () => {
   test("입력 금액이 숫자가 아니면 예외가 발생한다.", async () => {
@@ -9,6 +10,12 @@ describe("구매 금액 입력 테스트", () => {
     const app = new App({ input: new MockInput("500") });
     await expect(app.run()).rejects.toThrow(
       "[ERROR] 구입 금액은 1000원 단위로 입력해주세요.",
+    );
+  });
+  test("입력 금액이 양의 정수가 아니면 예외가 발생한다", async () => {
+    const app = new App({ input: new MockInput("3.14") });
+    await expect(app.run()).rejects.toThrow(
+      "[ERROR] 구입 금액은 양의 정수로 입력해주세요.",
     );
   });
 });
