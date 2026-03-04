5082ae209e7b44aed74bf75aeffbc6d4876d182f
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-03-04 17:14:54 +0900

    test: 로또 번호 개수 테스트 에러 수정

--- __tests__/lotto.test.js
+++ __tests__/lotto.test.js
@@ -1,16 +1,14 @@
-import Lotto from "../src/step1/app.js";
+import Lotto from "../src/step1/model/Lotto.js";
 import { LOTTO_ERROR_MESSAGE } from "../src/step1/constant/message.js";
 
 describe("로또 테스트", () => {
-  const lotto = new Lotto();
-
   test.each([
     [[1, 2, 3, 4, 5], "5개"],
     [[1, 2, 3, 4, 5, 45, 6], "7개"],
     [[], "0개"],
   ])("숫자가 6개 인가? (%s)", (numbers) => {
-    expect(() => new Lotto(numbers)).toThrow(
-      LOTTO_ERROR_MESSAGE.INPUT_NOT_SIX_NUMBERS,
-    );
+    expect(() => {
+      new Lotto(numbers);
+    }).toThrow(LOTTO_ERROR_MESSAGE.INPUT_NOT_SIX_NUMBERS);
   });
 });
