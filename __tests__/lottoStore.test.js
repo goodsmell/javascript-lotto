f621f10bac1767a642b2eb5f5660cb1bad7658f5
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-03-04 18:59:49 +0900

    test: 로또 발행 개수 테스트

---
+++ __tests__/lottoStore.test.js
@@ -0,0 +1,6 @@
+import LottoStore from "../src/step1/model/LottoStore";
+describe("로또 발행 테스트 ", () => {
+  test("로또 발행", () => {
+    expect(new LottoStore().issuedLotto(6).length).toBe(6);
+  });
+});
