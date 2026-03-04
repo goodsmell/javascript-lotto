93ca71133855970f0fbbefcb3000239b701f91cf
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-03-04 17:23:01 +0900

    feat: 로또 중복 검증 기능 구현

--- src/step1/model/Lotto.js
+++ src/step1/model/Lotto.js
@@ -3,12 +3,24 @@ import { LOTTO_COUNT } from "../constant/lotto.js";
 
 class Lotto {
   constructor(numbers) {
+    this.#validate(numbers);
+  }
+
+  #validate(numbers) {
     this.#validateCount(numbers);
+    this.#validateUniq(numbers);
   }
 
   #validateCount(numbers) {
     if (numbers.length !== LOTTO_COUNT) {
       throw new Error(LOTTO_ERROR_MESSAGE.INPUT_NOT_SIX_NUMBER);
+    }
+  }
+
+  #validateUniq(numbers) {
+    const n = new Set(numbers);
+    if (n.size !== numbers.length) {
+      throw new Error(LOTTO_ERROR_MESSAGE.INPUT_DUPLICATE);
     }
   }
 }
