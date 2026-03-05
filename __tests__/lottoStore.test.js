dc3f9d8008ab8ff42950e260a9edbe034d2fbfab
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-03-05 18:18:29 +0900

    fix: 확장자 추가

--- __tests__/lottoStore.test.js
+++ __tests__/lottoStore.test.js
@@ -1,4 +1,5 @@
-import LottoStore from "../src/step1/model/LottoStore";
+import LottoStore from "../src/step1/model/LottoStore.js";
+
 describe("로또 발행 테스트 ", () => {
   test("로또 발행", () => {
     expect(new LottoStore().issuedLotto(6).length).toBe(6);
