4bb50349ad944c9b676d8cf75ca9ecf275e67dfc
goodsmell <87801306+goodsmell@users.noreply.github.com>
2026-03-06 15:48:29 +0900

    test: 클래스 분리로 인한 테스트 수정

--- __tests__/getReturnOnInvestment.test.js
+++ __tests__/getReturnOnInvestment.test.js
@@ -1,16 +1,17 @@
 import { RANK } from "../src/step1/constant/index.js";
 import { getReturnOnInvestment } from "../src/step1/utils.js";
+import LottoResult from "../src/step1/model/LottoResult.js";
+import WinningLotto from "../src/step1/model/WinningLotto.js";
 
 describe("수익률 계산 테스트", () => {
   test("수익률을 정상적으로 계산하고있는가?", () => {
-    expect(
-      getReturnOnInvestment(5000, {
-        [RANK.FIRST]: 2,
-        [RANK.SECOND]: 1,
-        [RANK.THIRD]: 1,
-        [RANK.FOURTH]: 0,
-        [RANK.FIFTH]: 1,
-      }),
-    ).toBe(80630100);
+    const result = new LottoResult({
+      [RANK.FIRST]: 2,
+      [RANK.SECOND]: 1,
+      [RANK.THIRD]: 1,
+      [RANK.FOURTH]: 0,
+      [RANK.FIFTH]: 1,
+    });
+    expect(result.getReturnOnInvestment(5000)).toBe(80630100);
   });
 });
--- __tests__/winningLotto.test.js
+++ __tests__/winningLotto.test.js
@@ -1,6 +1,7 @@
 import WinningLotto from "../src/step1/model/WinningLotto.js";
 import { LOTTO_ERROR_MESSAGE } from "../src/step1/constant/message.js";
 import Lotto from "../src/step1/model/Lotto.js";
+import { RANK } from "../src/step1/constant/index.js";
 
 describe("보너스 번호 테스트", () => {
   test("당첨 번호 6개와 중복되는 경우 에러를 발생시키는 가?", () => {
@@ -14,4 +15,30 @@ describe("보너스 번호 테스트", () => {
       new WinningLotto(new Lotto([1, 2, 3, 4, 5, 46]));
     }).toThrow(LOTTO_ERROR_MESSAGE.INPUT_RANGE);
   });
+});
+
+describe("당첨 결과 계산", () => {
+  test.each([
+    ["6개", [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 7, RANK.FIRST],
+    [
+      "5개와 보너스 번호",
+      [1, 2, 3, 4, 5, 8],
+      [1, 2, 3, 4, 5, 7],
+      8,
+      RANK.SECOND,
+    ],
+    ["5개", [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 7], 8, RANK.THIRD],
+    ["4개", [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 7, 8], 9, RANK.FOURTH],
+    ["3개", [1, 2, 3, 4, 5, 6], [1, 2, 3, 7, 8, 9], 10, RANK.FIFTH],
+  ])(
+    "%s가 일치되는 경우 ",
+    (_, lottoNumbers, winningLottoNumbers, bonusNumber, rank) => {
+      const winningLotto = new WinningLotto(
+        new Lotto(winningLottoNumbers),
+        bonusNumber,
+      );
+      const result = winningLotto.evaluateLotto(new Lotto(lottoNumbers));
+      expect(result).toBe(rank);
+    },
+  );
 });
